aafaf7761d667c6ce497081547b7c3ddc7f75e24
pathompol ruchaudom <38848179+pathompol@users.noreply.github.com>
2022-01-17 22:19:43 +0700

    update ep 10
    update ep 10

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const chalk = require('chalk'); 
-const debug = require('debug')('server'); 
+const chalk = require('chalk');
+const debug = require('debug')('server');
 const morgan = require('morgan');
-const path =require('path');
+const path = require('path');
 
 app.use(morgan('combined'));
-app.use(express.static(path.join(__dirname,"/public/")));
+app.use(express.static(path.join(__dirname, "/public/")));
 app.get("/",(req,res) =>{
-    res.send('Hi pathompol Tee !!!!');
+
+    res.send('Hi !!!!');
 })
 
 
